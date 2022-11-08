3906cb1683a9841db8d4706b641a43f9b26adb18
aanddi <kuropatkinn.a.i21@gmail.com>
2022-11-08 21:37:19 +0300

    Создание маршрута сайта

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* Солнце */
+router.get('/sun', function(req, res, next) {
+  res.send("<h1>Солнце</h1>")
+});
+
+/* Земля */
+router.get('/earth', function(req, res, next) {
+  res.send("<h1>Земля</h1>")
+});
+
+/* Юпитер */
+router.get('/jupiter', function(req, res, next) {
+  res.send("<h1>Юпитер</h1>")
+});
+
+/* Марс */
+router.get('/mars', function(req, res, next) {
+  res.send("<h1>Марс</h1>")
+});
+
+/* Меркурий */
+router.get('/mercury', function(req, res, next) {
+  res.send("<h1>Меркурий</h1>")
+});
+
+/* Нептун */
+router.get('/neptune', function(req, res, next) {
+  res.send("<h1>Нептун</h1>")
+});
+
+/* Сатурн */
+router.get('/saturn', function(req, res, next) {
+  res.send("<h1>Сатурн</h1>")
+});
+
+/* Уран */
+router.get('/uranus', function(req, res, next) {
+  res.send("<h1>Уран</h1>")
+});
+
+/* Венера */
+router.get('/venus', function(req, res, next) {
+  res.send("<h1>Венера</h1>")
 });
 
 module.exports = router;

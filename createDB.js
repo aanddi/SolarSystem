245e430c21aec6ed6a2b4f05b3091627d65af24f
aanddi <kuropatkinn.a.i21@gmail.com>
2022-11-19 18:40:41 +0300

    Создание модуля данных

--- createDB.js
+++ createDB.js
@@ -1,15 +1,16 @@
 var MongoClient = require('mongodb').MongoClient
+var data = require("./data.js").data
 
 const uri = "mongodb://127.0.0.1:27017/"
 const client = new MongoClient(uri)
 async function run() {
    try {
       await client.connect();
-      var database = client.db("threecats");
+      var database = client.db("solarsystem");
       database.dropDatabase()
-      database = client.db("threecats");
-      const cats = database.collection("cats");
-      const result = await cats.insertOne({ name: "Барсик" });
+      database = client.db("solarsystem");
+      const planet = database.collection("planet");
+      const result = await planet.insertOne({ name: "Mars" });
       console.log(`${result} documents were inserted`);
    } finally {
       await client.close();

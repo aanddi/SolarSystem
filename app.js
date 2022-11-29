7bd3570105fd961d48f1521f688717555db744f0
aanddi <kuropatkinn.a.i21@gmail.com>
2022-11-29 13:58:15 +0300

    Установка модуля и настройка cookie

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: "ThreeCats",
+  secret: "SolarSystem",
   cookie:{maxAge:60*1000},
   resave: true,
   saveUninitialized: true	

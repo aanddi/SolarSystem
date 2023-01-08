c095072f0740963a0af9726a4c5320684c140f39
aanddi <kuropatkinn.a.i21@gmail.com>
2023-01-09 00:02:16 +0300

    Вывод данных на экран

--- routes/planets.js
+++ routes/planets.js
@@ -1,27 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var checkAuth = require("./../middleware/checkAuth.js")
-var Planet = require("../models/planet").Planet
-var async = require("async")
+var db = require('../mySQLConnect.js');
+//var checkAuth = require("./../middleware/checkAuth.js")
+//var Planet = require("../models/planet").Planet
+//var async = require("async")
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маошрутов, начинающихся с Planet');
-});
+router.get('/', function (req, res, next) {
+  res.send('Новый маршрутизатор, для маршрутов, начинающихся с planets');});
 
-/* Страница ведущих */
-router.get('/:nick' ,checkAuth, function(req, res, next) {
-  Planet.findOne({nick:req.params.nick}, function(err,planet){
-      if(err) return next(err)
-      if(!planet) return next(new Error("Нет такой планеты в солнечной системе"))
+/* Страница планет */
+router.get("/:nick", function (req, res, next) {
+  db.query(`SELECT * FROM planets WHERE planets.nick = '${req.params.nick}'`, (err, planets) => {
+    if (err) {
+      console.log(err);
+      if (err) return next(err)
+    } else {
+      if (planets.length == 0) return next(new Error("Нет такого котенка в мультике"))
+      var planet = planets[0];
       res.render('solsytem', {
         title: planet.title,
         picture: planet.avatar,
-        desc1: planet.desc1,
-        desc2: planet.desc2,
-        desc3: planet.desc3,
+        desc: planet.about,
       })
+    }
   })
-})
+
+});
 
 module.exports = router;
